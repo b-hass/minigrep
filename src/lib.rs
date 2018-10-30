39278f024ae2fafca00270f92a984dac945e9c18
Bruno Hass <bruno_hass@live.com>
2018-10-30 18:42:24 -0300

    Add test for the search function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
 use std::fs;
 use std::error::Error;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "bett";
+        let contents = "\
+Rust is
+much better than
+C++.";
+        assert_eq!(
+            vec!("much", "better", "than"),
+            search(query, contents)
+        );
+    }
+}
+
 pub struct Config {
     query: String,
     filename: String,
@@ -23,4 +41,8 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
        let contents = fs::read_to_string(&config.filename)?;
 
        Ok(())
+}
+
+fn search <'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
 }
